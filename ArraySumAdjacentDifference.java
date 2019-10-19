6172ea287fc75857402f14ca076dd223f6d04eca
setco-website <48595752+setco-website@users.noreply.github.com>
2019-10-19 21:13:12 +0100

    Indentation and rename.

---
+++ ArraySumAdjacentDifference.java
@@ -0,0 +1,7 @@
+public int arraySumAdjacentDifference(int[] in) {
+    int sum = 0;
+    for(int i = 1; i < in.length; i++) {
+        sum += Math.abs(in[i] - in[i-1]);
+    }
+    return sum;
+}
--- arraySumAdjacentDifference.java
+++
@@ -1,9 +0,0 @@
-public int arraySumAdjacentDifference(int[] in) {
-    
-    int sum=0;
-    for(int i=1;i<in.length;i++) {
-        sum += Math.abs(in[i]-in[i-1]);
-    }
-
-    return sum;
-}
