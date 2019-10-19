3005229e01772f2ea0e48548fa9221754f7a6e2b
dhanyaajila <dhanyashreeajilagmail.com>
2019-10-20 02:10:22 +0530

    adjecent difference function is written

--- arraySumAdjacentDifference.java
+++ arraySumAdjacentDifference.java
@@ -1,3 +1,9 @@
 int arraySumAdjacentDifference(int[] inputArray) {
-
+    int sum=0;
+    for(int i=0; i<inputArray.length-1; i++) {
+        if(inputArray[i]<inputArray[i+1])
+            sum+=inputArray[i+1]-inputArray[i];
+        else 
+            sum+=inputArray[i]-inputArray[i+1];
+    }
 }
