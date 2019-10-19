73985b670ddf4962a3691d0c793c404db7de7315
nemruddemir <nemrud_demir@hotmail.com>
2019-10-19 22:00:55 +0200

    arraySumAdjacentDifference implemented

--- arraySumAdjacentDifference.java
+++ arraySumAdjacentDifference.java
@@ -1,3 +1,7 @@
 int arraySumAdjacentDifference(int[] inputArray) {
-
+    int sum = 0;
+    for(int i = 1; i<inputArray.length; i++) {
+        sum += Math.abs(inputArray[i]-inputArray[i-1]);
+    }
+    return sum;
 }
